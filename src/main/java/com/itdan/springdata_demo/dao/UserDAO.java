c7900ca0632416f37e82d29cdfd878ddb75bc011
dan <2207161187@qq.com>
2019-04-03 17:01:24 +0800

    springdatademo

--- src/main/java/com/itdan/springdata_demo/dao/UserDAO.java
+++ src/main/java/com/itdan/springdata_demo/dao/UserDAO.java
@@ -3,7 +3,61 @@ package com.itdan.springdata_demo.dao;
 import com.itdan.springdata_demo.pojo.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+@Transactional
 //JpaRepository<User,Integer> 第一个参数：我们要操作的类，第二个参数：该类的主键是什么类型的
-public interface UserDAO extends JpaRepository<User,Integer> ,JpaSpecificationExecutor<User> {
+//JpaSpecificationExecutor该接口不能单独使用，要配合JpaRepository
+public interface UserDAO extends Repository<User,Integer>,
+        JpaRepository<User,Integer> ,
+        JpaSpecificationExecutor<User> {
+
+    /**
+     * 根据user_name 查询出user对象
+     * @param name
+     * @return
+     */
+    List<User> findByNameIs(String name);
+
+    /**
+     * 模糊查询
+     * @param keyword
+     * @return
+     */
+    List<User> findByNameLike(String keyword);
+
+    /**
+     * 条件查询
+     * @param name
+     * @param age
+     * @return
+     */
+    List<User> findByNameAndAgeGreaterThanEqual(String name,Integer age);
+
+
+    //使用Query标签查询
+    //nativeQuery:开启sql查询
+    @Query(value=" select * from user where u_name= ?",nativeQuery = true)
+    List<User> queryUserByNameUseJPQL(String name);
+
+    //模糊查询
+    @Query(value=" select * from user where u_name like ?",nativeQuery = true)
+    List<User> queryByLike(String keyword);
+
+    //更新语句
+    /*
+    * 异常： Executing an update/delete query
+    解决方案
+    因为jpa要求，’没有事务支持，不能执行更新和删除操作’。
+    所以反过来讲，就是在Service层或者Repository层上必须加@Transactional，
+    来代表这是一个事务级别的操作，增删改查除了查都是事务级别的，就当这是一个规范也是ok的。
+    * */
+    @Query(value = "UPDATE user set u_add=? where u_id=?",nativeQuery = true)
+    @Modifying
+    void updateUser(String add,Integer id);
 }
--- src/main/java/com/itdan/springdata_demo/test/MyTest.java
+++ src/main/java/com/itdan/springdata_demo/test/MyTest.java
@@ -9,9 +9,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = StartUpApplication.class)
@@ -36,6 +41,94 @@ public class MyTest {
 
     @Test
     public void testFindUser () throws Exception{
+        //根据name查询相应的User对象
+        //方法明名规则
+        //findBy（关键词）
+        //判断相等的三种形式,1.什么都不写默认就是相等;2.Is;3.Equals
+       List<User> users= userDAO.findByNameIs("小明");
+        for (User user :users) {
+            System.out.println(user);
+        }
+    }
+
+    @Test
+    public void testThan () throws Exception{
+     //条件：查询name=小明并且age>=10的
+         List<User> users= userDAO.findByNameAndAgeGreaterThanEqual("小明",20);
+        for (User user :users) {
+            System.out.println(user);
+        }
+    }
+
+    @Test
+    public void testLike () throws Exception{
+     //模糊查询(Like)
+       List<User> users=userDAO.findByNameLike("%小%");
+        for (User user :users) {
+            System.out.println(user);
+        }
+    }
+
+    @Test
+    public void testQueryByName () throws Exception{
+        //使用query注解查询
+     List<User> users= userDAO.queryUserByNameUseJPQL("小明");
+        for (User user :users) {
+            System.out.println(user);
+        }
+    }
+
+    @Test
+    public void testQueryLike () throws Exception{
+        //使用query注解模糊查询
+     List<User> users= userDAO.queryByLike("%小%");
+        for (User user :users) {
+            System.out.println(user);
+        }
+    }
+
+    @Test
+    public void testUpdateUser () throws Exception{
+       userDAO.updateUser("南宁",1);
+    }
+
+    @Test
+    public void testPage () throws Exception{
+     //分页查询
+        int page=0;//当前页，索引
+        int size=3;//每页显示条数
+        Pageable pageable=new PageRequest(page,size);
+        Page<User> users= userDAO.findAll(pageable);
+        for (User user :users) {
+            System.out.println(user);
+        }
+    }
+
+    @Test
+    public void testSortDemo01 () throws Exception{
+        //（测试失败）
+     //对象指定的列做排序
+     //Direction排序的规则
+     //properties指定做排序的属性(对应类中的字段，而不是表中的)
+        Sort sort=new Sort(Sort.Direction.DESC,"id");
+        List<User> users= userDAO.findAll(sort);
+        for (User user :users) {
+            System.out.println(user);
+        }
+    }
+
+    @Test
+    public void testSortDemo02 () throws Exception{
+        //对象多列排序做排序
+        //Direction排序的规则
+        //properties指定做排序的属性
+        Sort.Order order1=new Sort.Order(Sort.Direction.DESC,"id");
+        Sort.Order order2=new Sort.Order(Sort.Direction.ASC,"age");
+        Sort sort=new Sort(order1,order2);
+        List<User> users= userDAO.findAll(sort);
+        for (User user :users) {
+            System.out.println(user);
+        }
     }
 
 
